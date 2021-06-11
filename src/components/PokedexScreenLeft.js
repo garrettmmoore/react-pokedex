bef132e8de9c49bbeb24346b2fe27178059de526
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2021-06-11 14:47:04 -0700

    Fix aspect ratio of pokemon image
    Co-authored-by: Jacob Moore <jrmoore117@users.noreply.github.com>

--- src/components/PokedexScreenLeft.js
+++ src/components/PokedexScreenLeft.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { GridItem, Image, Box } from '@chakra-ui/react';
+import { GridItem, Image } from '@chakra-ui/react';
 
 const PokedexScreenLeft = props => {
   const { src } = props;
   return (
-    <Box>
-      <GridItem
-        colStart={2}
-        colEnd={9}
-        rowStart={1}
-        rowEnd={4}
-        className="flex-1 pokedex-screen w-full h-full"
-      >
-        <Image src={src} alt="" className="h-80 w-80" />
-      </GridItem>
-    </Box>
+    <GridItem
+      colStart={2}
+      colEnd={9}
+      rowStart={1}
+      rowEnd={4}
+      className="flex-1 bg-white rounded-lg"
+    >
+      <Image
+        src={src}
+        alt="pokemon"
+        className="h-full w-full"
+        objectFit="contain"
+      />
+    </GridItem>
   );
 };
 
